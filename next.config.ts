420e9d77156a7347cfbc1b4ca35aa018a0d8f329
Hussain-M53 <hash.hussain53@gmail.com>
2025-01-04 22:05:51 +0500

    eslint disabled

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['tailwindui.com', 'images.unsplash.com','media.istockphoto.com','www.shutterstock.com','encrypted-tbn0.gstatic.com','media.licdn.com']
   },

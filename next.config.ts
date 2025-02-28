f675c2be0e849a20d794a7102317a6d0c6861f92
Hussain-M53 <hash.hussain53@gmail.com>
2025-02-28 23:09:28 +0500

    bug fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
